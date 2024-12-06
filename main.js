c9453134e5a0acbae319c782d686c55250f3ce12
sd2k42 <p5nwr79etajfepiwgd4ahn3q9e4czs@gmail.com>
2024-12-05 23:25:14 -0700

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ document.addEventListener('mousemove', (e) => {
   const rotateY = ((e.clientX - (card.offsetLeft + card.offsetWidth / 2)) / card.offsetWidth) * 15.5;
   const moveX = (e.clientX - (card.offsetLeft + card.offsetWidth / 2)) / card.offsetWidth * -40;
   const moveY = (e.clientY - (card.offsetTop + card.offsetHeight / 2)) / card.offsetHeight * -40;
-  const scale = 1 + (Math.abs(moveX) + Math.abs(moveY)) / -2  000;
+  const scale = 1 + (Math.abs(moveX) + Math.abs(moveY)) / -2000;
   
   box.style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg) translateX(${moveX}px) translateY(${moveY}px) scale(${scale})`;
 });
