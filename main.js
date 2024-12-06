2de19ef4e541d7c1207437ffdcf3b4b7de641a53
sd2k42 <p5nwr79etajfepiwgd4ahn3q9e4czs@gmail.com>
2024-12-05 23:24:53 -0700

    Update main.js

--- main.js
+++ main.js
@@ -14,10 +14,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('mousemove', (e) => {
   const card = document.querySelector('.card');
-  const rotateX = ((e.clientY - (card.offsetHeight / 2 + card.offsetTop)) / card.offsetHeight) * 10.5;
-  const rotateY = ((e.clientX - (card.offsetWidth / 2 + card.offsetLeft)) / card.offsetWidth) * 15.5;
-  document.querySelector('.rotating-box').style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg)`;
+  const box = document.querySelector('.rotating-box');
+  const rotateX = ((e.clientY - (card.offsetTop + card.offsetHeight / 2)) / card.offsetHeight) * 10.5;
+  const rotateY = ((e.clientX - (card.offsetLeft + card.offsetWidth / 2)) / card.offsetWidth) * 15.5;
+  const moveX = (e.clientX - (card.offsetLeft + card.offsetWidth / 2)) / card.offsetWidth * -40;
+  const moveY = (e.clientY - (card.offsetTop + card.offsetHeight / 2)) / card.offsetHeight * -40;
+  const scale = 1 + (Math.abs(moveX) + Math.abs(moveY)) / -2  000;
+  
+  box.style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg) translateX(${moveX}px) translateY(${moveY}px) scale(${scale})`;
 });
+
 
 window.addEventListener('beforeunload', function (event) {
   if (clicked == true) {
