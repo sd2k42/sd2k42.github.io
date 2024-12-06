46ba85bf98be9cef8e95795a89f2b1b434d63b74
sd2k42 <p5nwr79etajfepiwgd4ahn3q9e4czs@gmail.com>
2024-12-05 20:28:09 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let clicked = false;
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('fbi').addEventListener('click', function(event) {
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('fbi').addEventListener('click', function (event) {
     event.preventDefault();
     document.getElementById('audio').play();
     document.getElementById('fbi').remove();
@@ -15,13 +15,20 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('mousemove', (e) => {
   const card = document.querySelector('.card');
   const rotateX = ((e.clientY - (card.offsetHeight / 2 + card.offsetTop)) / card.offsetHeight) * 10.5;
-  const rotateY = ((e.clientX - (card.offsetWidth / 2 + card.offsetLeft)) / card.offsetWidth) * 15.5; 
+  const rotateY = ((e.clientX - (card.offsetWidth / 2 + card.offsetLeft)) / card.offsetWidth) * 15.5;
   document.querySelector('.rotating-box').style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg)`;
 });
 
-window.addEventListener('beforeunload', function(event) {
+window.addEventListener('beforeunload', function (event) {
   if (clicked == true) {
     event.returnValue = '.';
     return '.';
+  }
+});
+
+document.addEventListener('fullscreenchange', async () => {
+  if (document.fullscreenElement) {
+    await navigator.keyboard.lock(['Escape', 'F11']);
+    return;
   }
 });
