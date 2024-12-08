56ec010c8be100d7f76203c1b309e3940028e8a5
sd2k42 <p5nwr79etajfepiwgd4ahn3q9e4czs@gmail.com>
2024-12-08 13:53:03 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-
-let clicked = false;
-
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('links').innerHTML += `
-            <a href="https://onlyfans.com/sd2k42" id="fbi">
-            <img src="common/of.svg" width="75vh" style="pointer-events: none;">
-          </a>`;
-  document.getElementById('body').innerHTML += `<audio id="audio" src="common/gooner.ogg"></audio>`;
-
-  document.getElementById('fbi').addEventListener('click', function (event) {
-    event.preventDefault();
-    document.getElementById('audio').play();
-    document.getElementById('fbi').remove();
-    clicked = true;
-    document.documentElement.requestFullscreen();
-    document.querySelector('.curtain').style.opacity = 1;
-    document.querySelector('.gooner').style.opacity = 1;
-    document.title = 'porn addict';
-    document.getElementById('html').classList.add('html');
-    document.querySelector('.card').remove();
-  });
-});
-
 document.addEventListener('mousemove', (e) => {
   const card = document.querySelector('.card');
   const box = document.querySelector('.rotating-box');
@@ -32,17 +8,4 @@ document.addEventListener('mousemove', (e) => {
   const scale = 1 + (Math.abs(moveX) + Math.abs(moveY)) / -2000;
   
   box.style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg) translateX(${moveX}px) translateY(${moveY}px) scale(${scale})`;
-});
-
-window.addEventListener('beforeunload', function (event) {
-  if (clicked == true) {
-    event.returnValue = '.';
-  }
-});
-
-document.addEventListener('fullscreenchange', async () => {
-  if (document.fullscreenElement) {
-    await navigator.keyboard.lock(['Escape', 'F11']);
-    return;
-  }
 });
